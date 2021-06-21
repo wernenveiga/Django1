bca121711302150df532ef0df3175710a2ffb133
Wernen Veiga <wernen_veiga@outlook.com>
2021-06-21 19:48:08 -0300

    verificação dos erros

--- django1/settings.py
+++ django1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+k$&$#ghj^p01at_rsgc*z8i3*6ha3zj-w+=#!x=@v$s54rc%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django1-wsv.herokuapp.com/']
 
